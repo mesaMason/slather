d316606a94d8f1bb750218f612f6079f566d4830
Mike <mbl2109@columbia.edu>
2016-09-20 14:47:01 -0400

    improved random walker: instead of randomly walking, acts like default player. also added an avoid radius to determine whether to move away from friendlies, instead of moving away from all friendlies in sight

--- g8/Player.java
+++ g8/Player.java
@@ -15,7 +15,8 @@ public class Player implements slather.sim.Player {
 
     private static final int NUMDIRECTIONS = 4;   // CONSTANTS - number of directions
     private static final int DURATION_CAP = 4;   // CONSTANTS - cap on traveling
-
+    private static final int FRIENDLY_AVOID_DIST = 5; // CONSTANTS - for random walker: turn away from friendlies that are in this radius
+    
     public void init(double d, int t) {
 	gen = new Random();
 	this.d = d;
@@ -25,10 +26,17 @@ public class Player implements slather.sim.Player {
     /*
       Memory byte setup:
       1 bit strategy (currently either square walker or random walker)
+
+      Memory byte for square walker:
+      1 bit strategy
       3 bits empty for now
       2 bits duration
       2 bits direction
       
+      Memory byte for random walker:
+      1 bit strategy
+      7 bits previous direction
+
       Directions:
       0 = North
       1 = East
@@ -58,19 +66,20 @@ public class Player implements slather.sim.Player {
             memory = writeDuration(duration, memory, maxDuration);
 
             Point destination = getNewDest(direction);
+
+            /*
             String s = String.format("%8s", Integer.toBinaryString(memory & 0xFF)).replace(' ','0');
             String p = String.format("current x is %f and current y is %f", currentPosition.x, currentPosition.y);
             String des = String.format("dest x is %f and dest y is %f", destination.x, destination.y);
-            /*
             System.out.println(s);
             System.out.println(p);
             System.out.println(des);
             */
-            // if collies, try 20 different random directions
+            
+            // if collides, try 20 different random directions
             if (collides(player_cell, destination, nearby_cells, nearby_pheromes)) {
-                System.out.println("WALKER COLLIDED");
                 for (int i = 0; i < 20; i++) {
-                    int arg = gen.nextInt(180);
+                    int arg = gen.nextInt(120);
                     Point vector = extractVectorFromAngle(arg);
                     if (!collides(player_cell, vector, nearby_cells, nearby_pheromes))
                         return new Move(vector, memory);
@@ -82,19 +91,23 @@ public class Player implements slather.sim.Player {
             return new Move(destination, memory);
         } else {
             /* Random walker strategy:
-               Move away from friendly cells
+               Move away from friendly cells that are too close (specified by FRENDLY_AVOID_DIST)
+               If no closeby friendly cells to avoid, act like default player (move in straight lines)
              */
+            int prevAngle = memory & 0b01111111;
 
-            // calculate avg positions of nearby friendly cells
             Iterator<Cell> cell_it = nearby_cells.iterator();
             double sumX = 0;
             double sumY = 0;
             int count = 0;
             Point vector;
-            
+
+            // calculate avg position of nearby friendly cells
             while (cell_it.hasNext()) {
                 Cell curr = cell_it.next();
                 if (curr.player != player_cell.player)
+                    continue;
+                if (player_cell.distance(curr) > FRIENDLY_AVOID_DIST) // don't worry about far away friendlies
                     continue;
                 Point currPos = curr.getPosition();
                 sumX += currPos.x;
@@ -102,10 +115,18 @@ public class Player implements slather.sim.Player {
                 count++;
             }
 
-            if (count == 0) {
-                int arg = gen.nextInt(90);
-                vector = extractVectorFromAngle(arg);
-            } else {
+            if (count == 0) { // case: no friendly cells to move away from
+                // if had a previous direction, keep going in that direction
+                if (prevAngle > 0) {
+                    vector = extractVectorFromAngle( (int)prevAngle);
+                    if (!collides( player_cell, vector, nearby_cells, nearby_pheromes))
+                        return new Move(vector, memory);
+                }
+
+                // if will collide or didn't have a previous direction, pick a random direction
+                prevAngle = gen.nextInt(120);
+                vector = extractVectorFromAngle(prevAngle);
+            } else { // case: friendly cells too close, move in opposite direction
                 double avgX = sumX / ((double) count);
                 double avgY = sumY / ((double) count);
 
@@ -117,21 +138,28 @@ public class Player implements slather.sim.Player {
                 double awayX = (-(towardsAvgX)/distanceFromAvg) * Cell.move_dist;
                 double awayY = (-(towardsAvgY)/distanceFromAvg) * Cell.move_dist;
 
+                // clear the previous vector bits
+                prevAngle = 0;
                 vector = new Point(awayX, awayY);
             }
             
             if (collides(player_cell, vector, nearby_cells, nearby_pheromes)) {
-                int arg = gen.nextInt(180);
+                int arg = gen.nextInt(120);
                 // try 20 times to avoid collision
                 for (int i = 0; i < 20; i++) {
                     vector = extractVectorFromAngle(arg);
-                    if (!collides(player_cell, vector, nearby_cells, nearby_pheromes))
+                    if (!collides(player_cell, vector, nearby_cells, nearby_pheromes)) {
+                        memory = (byte) (0b10000000 | arg);
                         return new Move(vector, memory);
+                    }
+
                 }
                 // if still keeps colliding, stay in place
-                return new Move(new Point(0,0), memory);
+                return new Move(new Point(0,0), (byte) 0b10000000);
             }
 
+            // didn't collide, recreate memory with new vector and move
+            memory = (byte) (0b10000000 | prevAngle);
             return new Move(vector, memory);
         }
     }
@@ -154,9 +182,9 @@ public class Player implements slather.sim.Player {
 	return false;
     }
     
-    // convert an angle (in 2-deg increments) to a vector with magnitude Cell.move_dist (max allowed movement distance)
+    // convert an angle (in 3-deg increments) to a vector with magnitude Cell.move_dist (max allowed movement distance)
     private Point extractVectorFromAngle(int arg) {
-	double theta = Math.toRadians( 2* (double)arg );
+	double theta = Math.toRadians( 3* (double)arg );
 	double dx = Cell.move_dist * Math.cos(theta);
 	double dy = Cell.move_dist * Math.sin(theta);
 	return new Point(dx, dy);
