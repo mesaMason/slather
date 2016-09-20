469c3cbb58f10f9baeaa994e4f6f9ad4bf393512
Mike <mbl2109@columbia.edu>
2016-09-20 13:52:15 -0400

    added new random walker strategy: moves away from friendly cells, if it can't see any moves randomly

--- g8/Player.java
+++ g8/Player.java
@@ -22,53 +22,118 @@ public class Player implements slather.sim.Player {
 	this.t = t;
     }
 
+    /*
+      Memory byte setup:
+      1 bit strategy (currently either square walker or random walker)
+      3 bits empty for now
+      2 bits duration
+      2 bits direction
+      
+      Directions:
+      0 = North
+      1 = East
+      2 = South
+      3 = West
+     */
     public Move play(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
 
         Point currentPosition  = player_cell.getPosition();
         int direction = getDirection(memory);
         int duration = getDuration(memory);
         int maxDuration = getMaxDuration(t, NUMDIRECTIONS);
+        int strategy = getStrategy(memory);
 
-        // 0 = North
-        // 1 = East
-        // 2 = South
-        // 3 = West
-
-        duration++;
-        if (duration == maxDuration) {
+	if (player_cell.getDiameter() >= 2) // reproduce whenever possible
+	    return new Move(true, (byte) 0b10000000, (byte) 0);
+        
+        if (strategy == 0) {
+            /* Square walker strategy
+             */
+            duration++;
+            if (duration == maxDuration) {
                 direction++;
                 direction = direction % NUMDIRECTIONS;
                 memory = writeDirection(direction, memory);
+            }
+            memory = writeDuration(duration, memory, maxDuration);
 
+            Point destination = getNewDest(direction);
+            String s = String.format("%8s", Integer.toBinaryString(memory & 0xFF)).replace(' ','0');
+            String p = String.format("current x is %f and current y is %f", currentPosition.x, currentPosition.y);
+            String des = String.format("dest x is %f and dest y is %f", destination.x, destination.y);
+            /*
+            System.out.println(s);
+            System.out.println(p);
+            System.out.println(des);
+            */
+            // if collies, try 20 different random directions
+            if (collides(player_cell, destination, nearby_cells, nearby_pheromes)) {
+                System.out.println("WALKER COLLIDED");
+                for (int i = 0; i < 20; i++) {
+                    int arg = gen.nextInt(180);
+                    Point vector = extractVectorFromAngle(arg);
+                    if (!collides(player_cell, vector, nearby_cells, nearby_pheromes))
+                        return new Move(vector, memory);
+                }
+                // if nothing worked, sit in place
+                return new Move(new Point(0,0), memory);
+            }
+            
+            return new Move(destination, memory);
+        } else {
+            /* Random walker strategy:
+               Move away from friendly cells
+             */
+
+            // calculate avg positions of nearby friendly cells
+            Iterator<Cell> cell_it = nearby_cells.iterator();
+            double sumX = 0;
+            double sumY = 0;
+            int count = 0;
+            Point vector;
+            
+            while (cell_it.hasNext()) {
+                Cell curr = cell_it.next();
+                if (curr.player != player_cell.player)
+                    continue;
+                Point currPos = curr.getPosition();
+                sumX += currPos.x;
+                sumY += currPos.y;
+                count++;
+            }
+
+            if (count == 0) {
+                int arg = gen.nextInt(90);
+                vector = extractVectorFromAngle(arg);
+            } else {
+                double avgX = sumX / ((double) count);
+                double avgY = sumY / ((double) count);
+
+                double towardsAvgX = avgX - currentPosition.x;
+                double towardsAvgY = avgY - currentPosition.y;
+
+                double distanceFromAvg = Math.hypot(towardsAvgX, towardsAvgY);
+            
+                double awayX = (-(towardsAvgX)/distanceFromAvg) * Cell.move_dist;
+                double awayY = (-(towardsAvgY)/distanceFromAvg) * Cell.move_dist;
+
+                vector = new Point(awayX, awayY);
+            }
+            
+            if (collides(player_cell, vector, nearby_cells, nearby_pheromes)) {
+                int arg = gen.nextInt(180);
+                // try 20 times to avoid collision
+                for (int i = 0; i < 20; i++) {
+                    vector = extractVectorFromAngle(arg);
+                    if (!collides(player_cell, vector, nearby_cells, nearby_pheromes))
+                        return new Move(vector, memory);
+                }
+                // if still keeps colliding, stay in place
+                return new Move(new Point(0,0), memory);
+            }
+
+            return new Move(vector, memory);
         }
-        memory = writeDuration(duration, memory, maxDuration);
-
-        Point destination = getNewDest(direction);
-        String s = String.format("%8s", Integer.toBinaryString(memory & 0xFF)).replace(' ','0');
-        String p = String.format("current x is %f and current y is %f", currentPosition.x, currentPosition.y);
-        String des = String.format("dest x is %f and dest y is %f", destination.x, destination.y);
-        System.out.println(s);
-        System.out.println(p);
-        System.out.println(des);
-        return new Move(destination, memory);
-
-        /*
-	if (player_cell.getDiameter() >= 2) // reproduce whenever possible
-	    return new Move(true, (byte)-1, (byte)-1);
-
-
-
-
-	// if no previous direction specified or if there was a collision, try random directions to go in until one doesn't collide
-	for (int i=0; i<4; i++) {
-	    int arg = gen.nextInt(180)+1;
-	    Point vector = extractVectorFromAngle(arg);
-	    if (!collides(player_cell, vector, nearby_cells, nearby_pheromes))
-		return new Move(vector, (byte) arg);
-	}
-
-	// if all tries fail, just chill in place
-	return new Move(new Point(0,0), (byte)0);*/
     }
 
     // check if moving player_cell by vector collides with any nearby cell or hostile pherome
@@ -83,12 +148,12 @@ public class Player implements slather.sim.Player {
 	Iterator<Pherome> pherome_it = nearby_pheromes.iterator();
 	while (pherome_it.hasNext()) {
 	    Pherome other = pherome_it.next();
-	    if (other.player != player_cell.player && destination.distance(other.getPosition()) < 0.5*player_cell.getDiameter() + 0.0001)
-		return true;
+            if (other.player != player_cell.player && destination.distance(other.getPosition()) < 0.5*player_cell.getDiameter() + 0.0001)
+                return true;
 	}
 	return false;
     }
-
+    
     // convert an angle (in 2-deg increments) to a vector with magnitude Cell.move_dist (max allowed movement distance)
     private Point extractVectorFromAngle(int arg) {
 	double theta = Math.toRadians( 2* (double)arg );
@@ -139,4 +204,8 @@ public class Player implements slather.sim.Player {
 
     }
 
+    private int getStrategy(byte memory) {
+        int strategy = (memory >> 7) & 1;
+        return strategy;
+    }
 }
