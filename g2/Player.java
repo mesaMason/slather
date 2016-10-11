07fa005aae77505f32927bffe26c725014823862
Stephanie Huang <syh2115@columbia.edu>
2016-10-10 23:55:02 -0400

    update with new player files

--- g2/Player.java
+++ g2/Player.java
@@ -6,80 +6,90 @@ import slather.sim.Move;
 import slather.sim.Pherome;
 import java.util.*;
 
+import slather.g2.util.Vector;
+
 public class Player implements slather.sim.Player {
 
-    private Random gen;
-    private int tailLength;
-    private double radius;
-    private double dTheta;
-    private double p_circle = 0.5;
-    private double size;
+    protected Random RANDOM_GENERATOR;
+    protected double VISION;
+    protected int TAIL_LENGTH;
+    protected double BOARD_SIZE;
+
+    protected double PROB_CIRCLE = 0.2;
+
+    private Chiller chiller = null;
+    private Circler occupier = null;
+    private Scout scout = null;
+
+    // Initialize here, initialize sub strategies here.
+    // Take care to initialize VISION, TAIL_LENGTH, BOARD_SIZE, RANDOM_GENERATOR in your subclass
+    // Do not make recursive calls to this init.
 
     public void init(double d, int t, int side_length) {
-        gen = new Random(System.currentTimeMillis());
-        tailLength = t;
-        radius = 2 * tailLength / (2*Math.PI);
-        dTheta = 1 / radius;
-        size = side_length;
+        System.out.println("Player init");
+
+        this.RANDOM_GENERATOR = new Random(System.currentTimeMillis());
+        this.VISION = d;
+        this.TAIL_LENGTH = t;
+        this.BOARD_SIZE = side_length;
+
+        if (t <= 4) PROB_CIRCLE = 0;
+
+        chiller = new Chiller();
+        chiller.init(d, t, side_length);
+
+        occupier = new Circler();
+        occupier.init(d, t, side_length);
+
+        scout = new Scout();
+        scout.init(d, t, side_length);
     }
 
-    public Move playCircle(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
-        //System.out.println("play circle");
-        double theta = byte2angle( memory );
-        double nextTheta = normalizeAngle(theta + dTheta,0);
-        byte nextMemory = 0;
-        Point vector = null;
-        // Try to go any of four normal directions.
-        for(int i=0; i<4; ++i) {
-            nextMemory = angle2byte( nextTheta, memory );
-            // System.out.println("memory,angles " +memory+","+nextMemory+"," +theta+","+nextTheta);
-            vector = angle2vector( nextTheta);
-            if (!collides( player_cell, vector, nearby_cells, nearby_pheromes))
-                return new Move(vector, nextMemory);
-            nextTheta += Math.PI/2;
-        }
-
-        // if all tries fail, just chill in place
-        return new Move(new Point(0,0), (byte)1);
+    public String move2String(Move m) {
+        return
+        "Move(" +m.reproduce
+            +","+m.vector.x
+            +","+m.vector.y
+            +","+String.format("%8s", Integer.toBinaryString(m.memory & 0xFF)).replace(' ', '0')
+            +","+String.format("%8s", Integer.toBinaryString(m.daughter_memory & 0xFF)).replace(' ', '0')
+            +")";
     }
 
-    //return true if there are more than one nearby friendly cell
-    private boolean crowded(Cell player_cell, Set<Cell> nearby_cells){
-        for(Cell c: nearby_cells){
-            if(c.player == player_cell.player && c.getPosition().distance(player_cell.getPosition()) < 2*radius) return true;
-        }
-        return false;
-    }
-
-    //by default goes scout;
-    //everytime we split there's a p_circle chance we go circle; 
     public Move play(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
-        double theta = byte2angle( memory );
         // reproduce whenever possible
+        Move m;
         if (player_cell.getDiameter() >= 2){
-            byte memory2 = angle2byte( normalizeAngle(theta + Math.PI,0), memory );
-            if(!byteIsCircle(memory) && (gen.nextDouble() < p_circle)) {
-                //System.out.println("Generating a circle");
-                memory2 = (byte) (memory2|ROLE_CIRCLE);
+            final double randomNumber = RANDOM_GENERATOR.nextDouble();
+            if(!byteIsCircle(memory) &&  (randomNumber< PROB_CIRCLE)) {
+                m = occupier.reproduce(player_cell, memory, nearby_cells, nearby_pheromes);
             } else {
-                //System.out.println("Generating a scout");
-                memory2 = (byte) (memory2& (~ROLE_CIRCLE));
+                m = scout.reproduce(player_cell, memory, nearby_cells, nearby_pheromes);
             }
-            return new Move(true, memory, memory2);
-        } 
+        } else {
+            boolean nextIsCircle = byteIsCircle(memory);
+            // nextIsCircle = false;
+            // if(scout.crowded(player_cell,nearby_cells)){
+            //     nextIsCircle = true;
+            // }
+            if(nextIsCircle){
+                // System.out.println("nextIsCircle");
+            }
+            if(!nextIsCircle) {
+                m = scout.play(player_cell, memory, nearby_cells, nearby_pheromes);
+            } else {
+                m = occupier.play(player_cell, memory, nearby_cells, nearby_pheromes);
+            }
+        }
 
-        //if surrounding is too crowded, play circle
-        boolean nextCircle = byteIsCircle(memory);
-        if(crowded(player_cell,nearby_cells)) nextCircle = true;
-        if(!nextCircle) 
-            return playScout(player_cell, memory, nearby_cells, nearby_pheromes);
-        return playCircle(player_cell, memory, nearby_cells, nearby_pheromes);
+        // System.out.println(move2String(m));
+
+        return m;
     }
 
     // check if moving player_cell by vector collides with any nearby cell or hostile pherome
-    private boolean collides(Cell player_cell, Point vector, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
+    protected boolean collides(Cell player_cell, Vector vector, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
         Iterator<Cell> cell_it = nearby_cells.iterator();
-        Point destination = player_cell.getPosition().move(vector);
+        Vector destination = new Vector( player_cell.getPosition().move(vector.getPoint()) );
         while (cell_it.hasNext()) {
             Cell other = cell_it.next();
             if ( destination.distance(other.getPosition()) < 0.5*player_cell.getDiameter() + 0.5*other.getDiameter() + 0.00011) 
@@ -94,147 +104,21 @@ public class Player implements slather.sim.Player {
         return false;
     }
 
-    private static final int ANGLE_BITS = 5;
-    private static final int ANGLE_MIN = 0;
-    private static final int ANGLE_MAX = 1 << ANGLE_BITS;
-    private static final int ANGLE_MASK = ANGLE_MAX - 1;
+    protected static final int ANGLE_BITS = 5;
+    protected static final int ANGLE_MIN = 0;
+    protected static final int ANGLE_MAX = 1 << ANGLE_BITS;
+    protected static final int ANGLE_MASK = ANGLE_MAX - 1;
 
-    private static final int ROLE_BITS = 1;
-    private static final int ROLE_MIN = ANGLE_MAX;
-    private static final int ROLE_MAX = ROLE_MIN + ( 1<<ROLE_BITS );
-    private static final int ROLE_MASK = (ROLE_MAX - 1) & ~ANGLE_MASK;
-    private static final int ROLE_SCOUT = 0 << ANGLE_BITS;
-    private static final int ROLE_CIRCLE = 1 << ANGLE_BITS;
+    protected static final int ROLE_BITS = 1;
+    protected static final int ROLE_MIN = ANGLE_MAX;
+    protected static final int ROLE_MAX = ROLE_MIN + ( 1<<ROLE_BITS );
+    protected static final int ROLE_MASK = (ROLE_MAX - 1) & ~ANGLE_MASK;
+    protected static final int ROLE_SCOUT = 0 << ANGLE_BITS;
+    protected static final int ROLE_CIRCLE = 1 << ANGLE_BITS;
 
     private boolean byteIsCircle(byte b){
         //System.out.printf("%d %d %d\n",b,ROLE_MASK,ROLE_CIRCLE);
         return (b&ROLE_MASK)==ROLE_CIRCLE;
-    }
-
-    private static final double TWOPI = 2*Math.PI;
-
-    private double byte2angle(byte b) {
-        // -128 <= b < 128
-        // -1 <= b/128 < 1
-        // -pi <= a < pi
-        return normalizeAngle(TWOPI * (((double) ((b) & ANGLE_MASK) ) / ANGLE_MAX), 0);
-    }
-
-    private byte angle2byte(double a, byte b) {
-        final double actualAngle = ((normalizeAngle(a,0) / TWOPI)*ANGLE_MAX);
-        final int anglePart = (int) (((int)actualAngle) & ANGLE_MASK);
-        final byte memoryPart = (byte) ( b & ~ANGLE_MASK);
-        // System.out.println("angle2byte "+ memoryPart +","+ anglePart +","+ (normalizeAngle(a,0)/TWOPI) +","+ ANGLE_MAX +","+ a);
-        return (byte) ((anglePart | memoryPart));
-    }
-
-    private double normalizeAngle(double a, double start) {
-        if( a < start ) {
-            return normalizeAngle( a+TWOPI, start);
-        } else if (a >= (start+TWOPI)) {
-            return normalizeAngle( a-TWOPI, start);
-        } else {
-            return a;
-        }
-    }
-
-    private Point angle2vector(double a) {
-        double dx = Cell.move_dist * Math.cos(a);
-        double dy = Cell.move_dist * Math.sin(a);
-        return new Point(dx, dy);
-    }
-
-    // convert an angle (in 2-deg increments) to a vector with magnitude Cell.move_dist (max allowed movement distance)
-    private Point extractVectorFromAngle(int arg) {
-        double theta = Math.toRadians( 2* (double)arg );
-        double dx = Cell.move_dist * Math.cos(theta + dTheta);
-        double dy = Cell.move_dist * Math.sin(theta + dTheta);
-        return new Point(dx, dy);
-    }
-
-
-     public Move playScout(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
-        //System.out.println("play scout");
-        if (player_cell.getDiameter() >= 2) // reproduce whenever possible
-            return new Move(true, (byte)-1, (byte)-1);
-        Point position = player_cell.getPosition();
-        double radius = player_cell.getDiameter() * 0.5;
-        Point direction = new Point(0, 0);
-        for (Cell cell : nearby_cells) {
-            Point p = cell.getPosition();
-            double r = cell.getDiameter() * 0.5 + radius;
-            double d = position.distance(p);
-
-            Point dir = correctedSubtract(p, position);
-
-            dir = normalize(dir);
-            if (Math.abs(d) > 1e-7)
-                dir = multiply(dir, weight(d, r));
-
-            direction = add(direction, multiply(dir, -1));
-        }
-        for (Pherome pherome : nearby_pheromes) {
-            Point p = pherome.getPosition();
-            double r = radius;
-            double d = position.distance(p);
-            
-            Point dir = correctedSubtract(p, position);
-
-            dir = normalize(dir);
-            if (Math.abs(d) > 1e-7)
-                dir = multiply(dir, weight(d, r));
-
-            direction = add(direction, multiply(dir, -1));
-        }
-        if (direction.norm() < 1e-8) {
-            double angle = gen.nextDouble() * 2 * Math.PI - Math.PI;
-            //double angle = 0;
-            direction = new Point(Math.cos(angle), Math.sin(angle));
-        }
-        direction = normalize(direction);
-        return new Move(direction, (byte)0);
-    }
-
-
-    private static double logGamma(double x) {
-      double tmp = (x - 0.5) * Math.log(x + 4.5) - (x + 4.5);
-      double ser = 1.0 + 76.18009173    / (x + 0)   - 86.50532033    / (x + 1)
-                       + 24.01409822    / (x + 2)   -  1.231739516   / (x + 3)
-                       +  0.00120858003 / (x + 4)   -  0.00000536382 / (x + 5);
-      return tmp + Math.log(ser * Math.sqrt(2 * Math.PI));
-   }
-   private static double gamma(double x) { return Math.exp(logGamma(x)); }
-
-    private double weight(double dist, double r) {
-        final double lambda = 4;
-
-        return Math.pow(lambda, dist) * Math.exp(-lambda) / gamma(dist);
-    }
-
-    private Point add(Point a, Point b) {
-        return new Point(a.x + b.x, a.y + b.y);
-    }
-
-    private Point subtract(Point a, Point b) {
-        return new Point(a.x - b.x, a.y - b.y);
-    }
-
-    private Point correctedSubtract(Point a, Point b) {
-        double x = a.x - b.x, y = a.y - b.y;
-        if (Math.abs(x) > Math.abs(a.x + size - b.x)) x = a.x + size - b.x;
-        if (Math.abs(x) > Math.abs(a.x - size - b.x)) x = a.x + size - b.x;
-        if (Math.abs(y) > Math.abs(a.y + size - b.x)) y = a.y + size - b.y;
-        if (Math.abs(y) > Math.abs(a.y - size - b.x)) y = a.y - size - b.y;
-        return new Point(x, y);
-    }
-
-    private Point multiply(Point a, double d) {
-        return new Point(a.x * d, a.y * d);
-    }
-
-    private Point normalize(Point a) {
-        if (a.norm() < 1e-7) return a;
-        else return multiply(a, 1.0/a.norm());
     }
 
 }
--- g7/Player.java
+++ g7/Player.java
@@ -13,28 +13,59 @@ public class Player implements slather.sim.Player {
 
 	private Random gen;
 	private Strategy strategy;
-	
-	public static int t=4;
+
+	public static int T;
+	public static double D;
+	public static int num_def_sides;
 
 	@Override
-	public void init(double d, int t) {
+	public void init(double d, int t, int side_length) {
+		T = t;
+		D = d;
 		gen = new Random();
 		strategy = new ClusterStrategy();
+		num_def_sides = Integer.min(16, Integer.max(4, T));
 	}
 
 	@Override
 	public Move play(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
-		if(nearby_pheromes.size()>0){
-			Iterator<Pherome> it=nearby_pheromes.iterator();
-			t=it.next().max_duration;
+		System.out.println("Memory is " + memory);
+		try {
+			// Check the type of cell
+			String memStr = DefenderMemory.byteToString(memory);
+			char defOrExp = memStr.charAt(0);
+			// System.out.println("Cell type flag: "+defOrExp);
+			/*
+			 * 0: Explorer 1: Defender
+			 */
+
+			// Convert the byte memory to binary string for easier usage
+
+			Memory m;
+			if (defOrExp == '0') {
+				ExplorerMemory thisMem = ExplorerMemory.getNewObject();
+				thisMem.initialize(memory);
+				m = thisMem;
+			} else if (defOrExp == '1') {
+				DefenderMemory thisMem = DefenderMemory.getNewObject();
+				thisMem.initialize(memory);
+				m = thisMem;
+			} else {
+				System.out.println("The cell is not recognized!");
+				m = DefenderMemory.getNewObject();
+			}
+
+			Move toTake = strategy.generateMove(player_cell, m, nearby_cells, nearby_pheromes);
+			System.out.println("New memory is " + toTake.memory);
+			return toTake;
+		} catch (Exception e) {
+			System.out.println("========= Exception in player =========");
+			e.printStackTrace();
+			return null;
 		}
+	}
 
-		// Convert the byte memory to binary string for easier usage
-		ExplorerMemory m = ExplorerMemory.getNewObject();
-		m.initialize(memory);
-
-		Move toTake = strategy.generateMove(player_cell, m, nearby_cells, nearby_pheromes);
-
-		return toTake;
+	public Strategy getStrategy() {
+		return strategy;
 	}
 }
